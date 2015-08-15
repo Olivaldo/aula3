ae794169357d4b3a96579ca7d6ac1739b9476477
Olivaldo Filho <olivaldo.filho@ernetwork.com.br>
2015-08-15 13:37:44 -0300

    Meu primeiro crud

--- core/views.py
+++ core/views.py
@@ -1,7 +1,9 @@
 # coding: utf-8
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect   
+from pessoa.models import Pessoa    
 
 # Create your views here.
 x = 1
 def index(request):
-    return render(request, 'index.html', {'msg': u'That is it'})
+    pessoas = Pessoa.objects.all()
+    return render(request, 'index.html', {'msg': u'That is it', 'pessoas':pessoas})
--- pessoa/models.py
+++ pessoa/models.py
@@ -6,3 +6,6 @@ class Pessoa(models.Model):
     nome = models.CharField(max_length=50, null=True)
     idade = models.IntegerField()
     ano = models.IntegerField()
+
+    def __unicode__(self):
+        return self.nome+' - '+str(self.idade)
