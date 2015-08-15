69e17c706d371f36d769826539d887976512c1c9
Olivaldo Filho <olivaldo.filho@ernetwork.com.br>
2015-08-15 10:26:43 -0300

    Aprendendo banco

--- pessoa/models.py
+++ pessoa/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=50, null=True)
+    idate = models.IntegerField(),
+
